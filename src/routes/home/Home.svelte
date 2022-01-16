5bbf3a057c0096ad3f27f382aa2d927af29ba97e
TotalLag <phanx068@gmail.com>
2022-01-16 00:08:48 -0600

    first commit

---
+++ src/routes/home/Home.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let data, helpers, settings;
+</script>
+
+<style global lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
+
+<svelte:head>
+  <title>Elder.js Template: Home</title>
+  <meta name="description" content="Elder.js Starter Template: It's A Tutorial Too!" />
+  <link href="{settings.origin}/" rel="canonical" />
+</svelte:head>
+
+<span class="text-3xl">HELLO WORLD</span>
